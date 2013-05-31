bd993742873250dd9dee38e189f56a72c84aab5e
Senjai <r.crawfordwilson@gmail.com>
2013-05-31 00:15:04 -0700

    Finished my CSV reader class

--- problem-1/lib/trans-reader.rb
+++ problem-1/lib/trans-reader.rb
@@ -1,26 +1,59 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
 require 'csv'
 
 module Trade
   class TransReader
-    def initialize(filename)
+    @@default_header = "store,sku,amount" 
+    def initialize(filename, header=@@default_header)
       @filename = filename
+      @header = header
       raise "File passed to Trade::TransReader does not exist" if !File.exist? filename
-      #_validate_trans_log
+      _validate_headers
     end
 
-    def get_transactions
+    def self.default_header
+      @@default_header
+    end
+
+    def self.default_header=(arg)
+      @@default_header = arg
+    end
+
+    def get_transactions(raw=false)
       begin
         trans_data = CSV.read(@filename)
       rescue Exception => e
         puts "Exception in get_transactions, #{e.message}"
       end
+      return trans_data if raw
       result = Hash.new
-      puts headers = trans_data.shift #so we can use this to store the headers manually because its the first line, the rest will be the actual data
-      p trans_data #so its an array of arrays, each index is a row. We can store this in the hash. Not sure what to use as the key. Or.. hmm.
+      trans_data.shift
+      trans_data.each do |arr|
+        (result[arr[1].intern] ||= []) << [arr[0], *_yank_currency(arr[2])] 
+      end
+      result
+    end
+
+    protected
+
+    def _validate_headers
+      first_line = ""
+      File.open('../data/sample_trans.csv', 'r') do |file|
+        first_line = file.gets.chomp
+      end
+      unless first_line == @header
+        raise "Error in TransReader::_validate_headers, invalid transaction log #{@filename}\n
+              Headers should be: #{@header} but was #{first_line}"        
+      end
+    end
+
+    def _yank_currency(amount)
+      amt, currency = amount.split(" ")
+      amt = amt.to_f
+      currency = currency.intern
+      return amt, currency
     end
   end
 end
-
-reader = Trade::TransReader.new('../data/sample_trans.csv')
-reader.get_transactions
 
